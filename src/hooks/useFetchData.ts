1ad7f8dae9440fe3216f94f1c06d962481d9110a
Joker <puebloseko@gmail.com>
2025-05-30 11:31:26 +0200

    security problem with the token solved

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,14 @@
+import { useEffect,useState } from "react";
+
+
+function useFetchData() {
+    
+const[data,setData] = useState(null);
+
+  return (
+    <div>useFetchData</div>
+  )
+}
+
+export default useFetchData
+
