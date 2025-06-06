e43cfadeb0e5157069a123d32844beba9f73d5fd
Joker <puebloseko@gmail.com>
2025-06-06 14:17:16 +0200

    WIP changing the API call by Redux

--- src/redux/fetchigSlice.ts
+++ src/redux/fetchigSlice.ts
@@ -1,35 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface initialState{
-    loading:boolean;
-    data:null | object;
-    error:boolean;
+interface initialState {
+  loading: boolean;
+  data: null | object;
+  error: boolean;
 }
 
-const initialState : initialState={
-
-loading:false,
-data:null,
-error:false
-
+const initialState: initialState = {
+  loading: false,
+  data: null,
+  error: false,
 };
 
-export const fetchSlice = createSlice(
- 
-    {
-        name:"fetch",
-        initialState,
-        reducers:{
-            fetchData:(state,action) =>{
-                
+export const fetchSlice = createSlice({
+  name: "fetch",
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
 
-            }
-        }
-    }
-
-
-
-
-
-);
-
+    fetchSucces: (state, action) => {
+      state.data = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    fetchFailure: (state) => {
+      state.error = true;
+      state.loading = false;
+    },
+  },
+});
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-export const store= configureStore(
+const store= configureStore(
 
     {
       reducer:{
@@ -12,3 +12,5 @@ export const store= configureStore(
 
 
 )
+
+export default store;
