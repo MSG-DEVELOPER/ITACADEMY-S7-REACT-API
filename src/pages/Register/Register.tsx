b9aca4fbf76f4c80c85a33a6241a79e5ed8c8e55
Joker <puebloseko@gmail.com>
2025-06-12 13:24:54 +0200

    Layout and styles of register page done. WIP validatios of the form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,5 @@
+import { useForm } from "react-hook-form";
+
 import {
   RegisterWrapper,
   Title,
@@ -6,16 +8,59 @@ import {
   SubmitButton,
 } from "./Register.style";
 
+type FormData = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 function Register() {
+  const { register, handleSubmit, formState } = useForm<FormData>();
+  const { errors } = formState;
+
+  function validateForm(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <RegisterWrapper>
       <Title>Crear cuenta</Title>
 
-      <Form>
-        <Input type="text" name="username" placeholder="Nombre de usuario" />
-        <Input type="email" name="email" placeholder="Email" />
-        <Input type="password" name="password" placeholder="Contraseña" />
-        <Input type="password" name="confirmPassword" placeholder="Confirmar contraseña" />
+      <Form onSubmit={handleSubmit(validateForm)}>
+        <Input
+          type="text"
+          placeholder="Username"
+          {...register("username", { required: "*Obligatorio" })}
+        />
+        {errors.username && <p>{errors.username.message}</p>}
+
+        <Input
+          type="email"
+          placeholder="Email"
+          {...register("email", {
+            required: "*Obligatorio",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Email no válido",
+            },
+          })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+
+        <Input
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: "*Obligatorio" })}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+
+        <Input
+          type="password"
+          placeholder="Confirm Password"
+          {...register("confirmPassword", { required: "*Obligatorio" })}
+        />
+        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
 
         <SubmitButton type="submit">Registrarse</SubmitButton>
       </Form>
