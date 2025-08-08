d2cdd980d1d66a1860bd1b7a62a7b7d77efe0dc1
Joker <puebloseko@gmail.com>
2025-08-08 19:24:46 +0200

    WIP with deploy in Vercel

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,9 @@
-
 import { useForm } from "react-hook-form";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../bbdd/config";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/userSlice";
-import { useLocation,useNavigate } from "react-router-dom";
-
-
+import { useLocation, useNavigate } from "react-router-dom";
 
 import {
   RegisterWrapper,
@@ -17,56 +14,62 @@ import {
 } from "../Register/Register.style";
 
 type FormData = {
-email:string;
-password:string;
-}
-
-
+  email: string;
+  password: string;
+};
 
 function Login() {
+  const { register, handleSubmit, formState, reset } = useForm<FormData>();
+  const { errors } = formState;
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const urlDestiy = location.state?.from?.pathname || "/";
 
-const {register,handleSubmit,formState,reset} =useForm<FormData>();          
-const {errors } = formState;
-const dispatch = useDispatch();
-const location=useLocation();
-const navigate=useNavigate();
-const urlDestiy= location.state?.from?.pathname ||"/";
+  async function onSubmit(data: FormData) {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      const user = userCredential.user;
 
+      dispatch(
+        login({
+          email: user.email,
+          uid: user.uid,
+        })
+      );
 
-async function onSubmit(data:FormData){
-
-  try{
-    const userCredential = await signInWithEmailAndPassword(auth,data.email,data.password);
-    const user = userCredential.user;
-    
-    dispatch(login({
-      email:user.email,
-      uid:user.uid
-    }))
-
-    reset();
-    navigate(urlDestiy,{replace:true});
-  }catch(error){
-
-    alert("❌ Error al iniciar sesión => " + error.message);
-
+      reset();
+      navigate(urlDestiy, { replace: true });
+    } catch (error) {
+      if (error instanceof Error) {
+        alert("❌ Error al iniciar sesión => " + error.message);
+      } else {
+        alert("❌ Error desconocido al iniciar sesión");
+      }
+    }
   }
-
-
-}
-
-
-
 
   return (
     <RegisterWrapper>
       <Title>Iniciar sesión</Title>
 
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <Input type="email" placeholder="Email" {...register("email",{required:"campo obligatorio"})} />
-        {errors.email&&<p>{errors.email.message}</p>}
+        <Input
+          type="email"
+          placeholder="Email"
+          {...register("email", { required: "campo obligatorio" })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
 
-        <Input type="password" placeholder="Password" {...register("password", { required: "*Campo obligatorio" })}/>
+        <Input
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: "*Campo obligatorio" })}
+        />
         {errors.password && <p>{errors.password.message}</p>}
 
         <SubmitButton type="submit">Entrar</SubmitButton>
--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,21 +1,17 @@
 import type { ReactElement } from "react";
 import { useSelector } from "react-redux";
-import { Navigate,useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
+import type { RootState } from "../redux/store"; 
 
+function ProtectedRoutes(props: { children: ReactElement }) {
+  const user = useSelector((state: RootState) => state.user);
+  const location = useLocation();
 
-function ProtectedRoutes(props: {children:ReactElement}) {
+  if (!user.email) {
+    return <Navigate to="/Login" replace state={{ from: location }} />;
+  }
 
-const user=useSelector((state:unknow)=>state.user);
-const location=useLocation();
-
-if(!user.email){
-    return <Navigate to="/Login" replace state={{from:location}} />
+  return <>{props.children}</>;
 }
 
-  return (
-  <> {props.children}</> 
- 
-  )
-}
-
-export default ProtectedRoutes
+export default ProtectedRoutes;
