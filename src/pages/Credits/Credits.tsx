cac9863d66bd297cfe5177ffd3b3b16918071ba0
Joker <puebloseko@gmail.com>
2025-08-08 19:09:21 +0200

    WIP solving ts problems to deploy in vercel

--- src/pages/Credits/Credits.tsx
+++ src/pages/Credits/Credits.tsx
@@ -7,14 +7,24 @@ import Spinner from "../../components/Shared/Spinner/Spinner";
 import NotFound from "../../components/Shared/NotFound/NotFound";
 import CardCredits from "../../components/Credits/CardCredits/CardCredits";
 import { PageWrapper, Grid } from "./Credits.style";
+import type { RootState, AppDispatch } from "../../redux/store";
 
-
+interface CastMember {
+  id: number;
+  name: string;
+  profile_path: string;
+  character: string;
+}
 
 function Credits() {
   const id = useParams().filmId;
   const url = `${urlCreditBase}${id}/credits?language=en-US`;
-  const { loading, data, error } = useSelector((state) => state.credits);
-  const dispatch = useDispatch();
+
+  const { loading, data, error } = useSelector(
+    (state: RootState) => state.credits
+  ) as { loading: boolean; data: { cast: CastMember[] } | null; error: boolean };
+
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchCreditsThunk(url));
@@ -24,7 +34,7 @@ function Credits() {
     <div>
       <PageWrapper>
         <Grid>
-          {data?.cast.map((member: any) => (
+          {data?.cast.map((member: CastMember) => (
             <CardCredits
               key={member.id}
               name={member.name}
--- src/pages/Films/Films.tsx
+++ src/pages/Films/Films.tsx
@@ -6,41 +6,43 @@ import { fetchThunk } from "../../redux/thunks/fetchThunk";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Body } from "./Films.style";
+import type { RootState, AppDispatch } from "../../redux/store";
 
-
-
+interface Film {
+  id: number;
+  title: string;
+  poster_path: string;
+  vote_average: number;
+}
 
 function Films() {
+  const { loading, data, error } = useSelector(
+    (state: RootState) => state.fetch
+  ) as { loading: boolean; data: { results: Film[] } | null; error: boolean };
 
-const { loading, data, error } = useSelector((state) => state.fetch);
-const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
+  useEffect(() => {
+    dispatch(fetchThunk(urlAllFilms));
+  }, [dispatch]);
 
-useEffect( ()=>{
-
-  dispatch(fetchThunk(urlAllFilms));
- 
-  
-
-} ,[dispatch])
-
-
- 
-
+  console.log(data);
 
   return (
     <Body>
-      {data?.results.map((item: object) => (
-        <CardFilms key={item.id} title={item.title} path={item.poster_path} vote={item.vote_average} 
-        $top={item.vote_average > 7.5} id={item.id}  />
-      ))}   
+      {data?.results.map((item: Film) => (
+        <CardFilms
+          key={item.id}
+          title={item.title}
+          path={item.poster_path}
+          vote={item.vote_average}
+          $top={item.vote_average > 7.5}
+          id={item.id.toString()}  // <-- convertimos a string aquí
+        />
+      ))}
 
-      {loading && <Spinner/> }
-      {error && <NotFound/>}
-      {console.log(data)}
-
-    
-
+      {loading && <Spinner />}
+      {error && <NotFound />}
     </Body>
   );
 }
